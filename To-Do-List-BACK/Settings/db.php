7b54304fd2794ad15095c121f6d8a2bebe6fa3b0
mariecor <mariaeugcoronel@gmail.com>
2025-11-12 20:38:07 -0300

    agrego bd y conexión

--- To-Do-List-BACK/Settings/db.php
+++ To-Do-List-BACK/Settings/db.php
@@ -1,4 +1,19 @@
 <?php
 
-class DB{}
+class DB{
+    protected static $host;
+    protected static $user;
+    protected static $pass;
+    protected static $db_name;
+
+    public static function dbConnect(){
+        self::$host = 'localhost';
+        self::$user = 'root';
+        self::$pass = '';
+        self::$db_name = 'tareas_tp';
+
+        $con = new mysqli(self::$host,self::$user,self::$pass,self::$db_name) or die ('Error en la conexión de la base de datos');
+        return $con;
+    }
+}
 ?>
---
+++ To-Do-List-BACK/db/tareas_tp.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-11-2025 a las 00:32:54
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tareas_tp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tareas`
+--
+
+CREATE TABLE `tareas` (
+  `id_tareas` int(11) NOT NULL,
+  `nombreT` varchar(255) NOT NULL,
+  `descripcion` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tareas`
+--
+ALTER TABLE `tareas`
+  ADD PRIMARY KEY (`id_tareas`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tareas`
+--
+ALTER TABLE `tareas`
+  MODIFY `id_tareas` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
